d5b8fc9488dccd69887090819e3137169ed69dcf
WILSON DERRICK LUCAS <t9y6@ssh-linux5.ece.ubc.ca>
2015-03-28 19:03:24 -0700

    divider.v coded

--- specification/divider.v
+++ specification/divider.v
@@ -4,8 +4,8 @@
 // This is the stub for your top-level block.  Please start with this
 // template when writing your Verilog code.
 //
-// Names:  <insert your names here>
-// Number:  <insert your student numbers here>
+// Names:  Derrick Wilson
+// Number:  76674068
 //
 
 
@@ -28,9 +28,36 @@ input start;
 input clk;
 input reset;
 
+wire load_w;
+wire add_w;
+wire sign_w;
+wire [1:0] sel_w;
+wire shift_w;
+wire inbit_w;
 
--- Insert your code here (you should just be including
--- datapath and control subblocks.  Unlikely to be any logic
--- in this file)
+// Structural Description
+controller controller(	load_w,		// output
+						add_w,		// output
+						shift_w,		// output
+						inbit_w,		// output
+						sel_w,		// output (2 bits)
+						valid,		// output
+						start,		// input
+						sign_w, 		// input
+						clk,		// clock input
+						reset);		// async reset
+
+datapath datapath(	remainder,		// output (7 bits)
+					quotient, 		// output (8 bits)
+					sign_w,			// output 
+					divisorin,		// input (7 bits)
+					dividendin,		// input (8 bits)
+					load_w,			// input
+					add_w,			// input
+					shift_w,			// input
+					inbit_w,			// input
+					sel_w,			// input (2 bits)
+					clk,			// clock input
+					reset);			// async reset 
 
 endmodule
