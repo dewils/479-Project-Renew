949da380813ff897b96029e0e8fa29a0f618d6cb
WILSON DERRICK LUCAS <t9y6@ssh-linux5.ece.ubc.ca>
2015-04-04 13:12:32 -0700

    add/sub transistor level finished and simmed

---
+++ cells/addsub1_transistorlevel.cmd
@@ -0,0 +1,27 @@
+stepsize 50
+analyzer X Z A B C
+vector in A B C
+set in 000
+s
+set in 000
+s
+set in 000
+s
+set in 001
+s
+set in 010
+s
+set in 011
+s
+set in 100
+s
+set in 101
+s
+set in 110
+s
+set in 111
+s
+set in 000
+s
+set in 000
+s
