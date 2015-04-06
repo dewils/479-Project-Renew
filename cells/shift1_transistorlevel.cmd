a121bc4d478d3f6869208c3dbcc359d62f6a1c1a
WILSON DERRICK LUCAS <t9y6@ssh-linux5.ece.ubc.ca>
2015-04-05 20:55:22 -0700

    shift1 transistorlevel completed and simulated

---
+++ cells/shift1_transistorlevel.cmd
@@ -0,0 +1,31 @@
+stepsize 50
+analyzer B A inbit shift noshift
+vector op noshift shift
+l A
+l inbit
+set op 10
+s
+h A
+l inbit
+set op 10
+s
+l A
+l inbit
+set op 10
+s
+l A
+h inbit
+set op 01
+s
+l A
+l inbit
+set op 10
+s
+l A
+l inbit
+set op 01
+s
+l A
+l inbit
+set op 10
+s
