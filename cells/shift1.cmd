a1099190375dd96d10cf643a539b36404db1b1eb
WILSON DERRICK LUCAS <t9y6@ssh-linux5.ece.ubc.ca>
2015-04-12 12:57:03 -0700

    fixed shift1 I/O

---
+++ cells/shift1.cmd
@@ -0,0 +1,36 @@
+stepsize 50
+analyzer B A shift inbit
+vector op shift_n shift
+w  B inbit A shift
+l A
+l inbit
+set op 10
+s
+h A
+l inbit
+set op 10
+s
+l A
+l inbit
+set op 10
+s
+l A
+h inbit
+set op 01
+s
+l A
+l inbit
+set op 10
+s
+l A
+l inbit
+set op 01
+s
+h A
+l inbit
+set op 10
+s
+h A
+l inbit
+set op 01
+s
