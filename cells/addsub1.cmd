42edd4d423ad38eb9fce506f99406f7ab6e3f06d
WILSON DERRICK LUCAS <t9y6@ssh-linux8.ece.ubc.ca>
2015-04-12 04:05:12 -0700

    addsub fixed I/O

---
+++ cells/addsub1.cmd
@@ -0,0 +1,30 @@
+stepsize 50
+analyzer X Z A B C add_n
+vector in A B C
+l add_n
+set in 000
+w Z X add_n C B Amagic
+s
+set in 001
+s
+set in 010
+s
+set in 011
+s
+set in 100
+s
+set in 101
+s
+set in 110
+s
+set in 111
+s
+h add_n
+set in 001
+s
+set in 011
+s
+set in 101
+s
+set in 111
+s
