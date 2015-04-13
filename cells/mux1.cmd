f715da8365ecbcd8d9fb7f8f99cfdf68adc11139
WILSON DERRICK LUCAS <t9y6@ssh-linux5.ece.ubc.ca>
2015-04-12 21:39:22 -0700

    fixed shifter

--- cells/mux1.cmd
+++ cells/mux1.cmd
@@ -1,46 +1,86 @@
 stepsize 1000
 analyzer Y S0 S1 D0 D1 D2
-vector S0_bus S0 S0_n 
-vector S1_bus S1 S1_n
+vector sel S0 S0_n S1 S1_n 
 vector D D0 D1 D2
 w Y S0 S1 D
-set D 100
-set S0_bus 01
-set S1_bus 01
+set D 000
+set sel 0101
 s
-set S0_bus 01
-set S1_bus 10
+set sel 0110
 s
-set S0_bus 10
-set S1_bus 01
+set sel 1001
 s
-set S0_bus 10
-set S1_bus 10
-s
-set D 010
-set S0_bus 01
-set S1_bus 01
-s
-set S0_bus 01
-set S1_bus 10
-s
-set S0_bus 10
-set S1_bus 01
-s
-set S0_bus 10
-set S1_bus 10
+set sel 1010
 s
 set D 001
-set S0_bus 01
-set S1_bus 01
+set sel 0101
 s
-set S0_bus 01
-set S1_bus 10
+set sel 0110
 s
-set S0_bus 10
-set S1_bus 01
+set sel 1001
 s
-set S0_bus 10
-set S1_bus 10
+set sel 1010
 s
-
+set D 010
+set sel 0101
+s
+set sel 0110
+s
+set sel 1001
+s
+set sel 1010
+s
+set D 011
+set sel 0101
+s
+set sel 0110
+s
+set sel 1001
+s
+set sel 1010
+s
+set D 100
+set sel 0101
+s
+set sel 0110
+s
+set sel 1001
+s
+set sel 1010
+s
+set D 101
+set sel 0101
+s
+set sel 0110
+s
+set sel 1001
+s
+set sel 1010
+s
+set D 110
+set sel 0101
+s
+set sel 0110
+s
+set sel 1001
+s
+set sel 1010
+s
+set D 111
+set sel 0101
+s
+set sel 0110
+s
+set sel 1001
+s
+set sel 1010
+s
+set D 000
+set sel 0101
+s
+set sel 0110
+s
+set sel 1001
+s
+set sel 1010
+s
--- cells/shift1.cmd
+++ cells/shift1.cmd
@@ -1,7 +1,7 @@
 stepsize 1000
-analyzer B outbit A shift inbit
+analyzer B A shift inbit
 vector op shift_n shift
-w  B outbit inbit A shift
+w  B inbit A shift
 l A
 l inbit
 set op 10
