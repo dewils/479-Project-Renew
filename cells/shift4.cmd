2a47f3c93472dbb83671aa7e1f2081d92bedfc27
WILSON DERRICK LUCAS <t9y6@ssh-linux5.ece.ubc.ca>
2015-04-12 22:38:58 -0700

    shifter fixed

---
+++ cells/shift4.cmd
@@ -0,0 +1,30 @@
+stepsize 1000
+
+| Set up vectors
+
+
+vector In A_{3:0}
+vector Out B_{3:0}
+
+analyzer Out In shift inbit
+w Out In shift inbit
+
+s
+l shift
+l inbit
+set In 0000
+s
+h inbit
+s
+h shift
+s
+l inbit
+s
+l shift
+s
+set IN 0101
+s
+h shift
+s
+l shift
+s
