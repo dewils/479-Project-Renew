035ec1343328064d26203adf26b5fa89683fbbb8
WILSON DERRICK LUCAS <t9y6@ssh-linux5.ece.ubc.ca>
2015-04-12 17:18:46 -0700

    dbitlow layed out and tested, shifter operation not sure about yet

--- layout/dbitlow.cmd
+++ layout/dbitlow.cmd
@@ -2,40 +2,97 @@ stepsize 1000
 
 | Set up vectors
 
-analyzer quotient dividendin D1 D2 S0 S1  
-vector sel S0 S0_n S1 S1_n
+analyzer quotient outbit dividendin S0 S1 inbit shift clk reset_n
+vector sel S1 S1_n S0 S0_n
+vector sft shift shift_n
+vector clock clk clk_n
+
 s
-set sel 0101
+set clock 10
+s
+set clock 01
+s
+l reset_n
+set clock 10
+s
+set clock 01
+s
+set clock 10
+s
+h reset_n
 l dividendin
-l D1
-l D2
-w S1 S0 dividendin D1 D2 quotient
+l inbit
+set sft 01
+set sel 0101
+w quotient outbit dividendin S0 S1 inbit shift clk reset_n
+set clock 01
+s
+set clock 10
 s
 h dividendin
-l D1
-l D2
+set clock 01
+s
+set clock 10
 s
 l dividendin
-l D1
-l D2
+set clock 01
+s
+set clock 10
+s
+| test mux
+h dividendin
+set clock 01
+s
+set clock 10
 s
 set sel 0110
-l dividendin
-h D1
-l D2
+set clock 01
+s
+set clock 10
 s
 l dividendin
-l D1
-l D2
+set clock 01
+s
+set clock 10
 s
 set sel 1001
-l dividendin
-l D1
-h D2
+set clock 01
 s
-l dividendin
-l D1
-l D2
+set clock 10
 s
-
+set clock 01
+| test shifter
+set sel 0101
+s
+set clock 10
+s
+set clock 01
+s
+set clock 10
+s
+set clock 01
+set sel 0110
+s
+set clock 10
+s
+set clock 01
+h inbit
+s
+set clock 10
+s
+set clock 01
+set sft 10
+s
+set clock 10
+s
+set clock 01
+s
+set clock 10
+s
+set clock 01
+s
+set clock 10
+s
+set clock 01
+s
 
